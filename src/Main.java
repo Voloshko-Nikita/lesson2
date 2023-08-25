7d3c75af7e15b99ac3463db2df941f63881df852
Voloshko-Nikita <nikita.voloshko.music@gmail.com>
2023-08-25 11:12:24 +0300

    Lesson 2 First homework. The program determines which of three values is greater. Second homework excersise done

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("-----Homework 1 ");
+        System.out.println("-----Homework 1");
 
         int a = 5, b = 19, c = 15;
         int result = a;
@@ -10,6 +10,35 @@ public class Main {
         if (c > result){
             result = c;
         }
+        System.out.println(result);
+
+
+        System.out.println("-----Homework ");
+
+        int x = 5, y = -7, m = 0;
+        result = 0;
+        if (y < 0){
+            y = -y;
+            m = -1;
+        }
+        else{
+            m = 1;
+        }
+
+        System.out.println(m + ", " + y);
+
+        while (y > 0){
+            if (y % 2 == 0){
+                x = x + x;
+                y = y / 2;
+            }
+            else{
+                result = result + x;
+                y = y - 1;
+            }
+        }
+        if (m < 0)
+            result = -result;
         System.out.println(result);
     }
 }
